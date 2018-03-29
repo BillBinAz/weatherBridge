d9fb204fd857d54de29fca19c1c2eeb06cf2b55a
BillB <github@billbennett.org>
2018-03-29 11:00:14 -0700

    Initial Push

--- bridge/main.py
+++ bridge/main.py
@@ -2,20 +2,39 @@ import xml.etree.ElementTree
 
 import httplib2
 
+
+def push_temp(variable_type, variable_id, c_temp):
+	f_temp = round(9.0 / 5.0 * float(c_temp) + 32, 2)
+	http_object = httplib2.Http()
+	http_object.add_credentials(user_mame, password)  # Basic authentication
+	resp, meteo_content = http_object. \
+		request("http://meteobridge/cgi-bin/livedataxml.cgi", "GET")
+
+	print(c_temp, f_temp)
+
+
 #
 # Get the username/password from the secret file
-secretFile = open("../secret/meteobridge", "r")
-userName = secretFile.readline().strip('\n')
-password = secretFile.readline().strip('\n')
+secret_file = open("../secret/meteobridge", "r")
+user_mame = secret_file.readline().strip('\n')
+password = secret_file.readline().strip('\n')
 
 #
 # Pull the XML from meteobridge
-httpObject = httplib2.Http()
-httpObject.add_credentials(userName, password)  # Basic authentication
-resp, meteoContent = httpObject. \
-  request("http://meteobridge/cgi-bin/livedataxml.cgi", "GET")
+http_object = httplib2.Http()
+http_object.add_credentials(user_mame, password)  # Basic authentication
+resp, meteo_content = http_object. \
+	request("http://meteobridge/cgi-bin/livedataxml.cgi", "GET")
 
 #
 # Process the XML Variables
-meteoHubXML = xml.etree.ElementTree.parse(meteoContent).getroot()
-
+meteobridge_xml = xml.etree.ElementTree.fromstring(meteo_content)
+for sensor in meteobridge_xml.findall('TH'):
+	if sensor.get('id') == "th6":
+		push_temp(2, 1, sensor.get('temp'))
+	elif sensor.get('id') == "th0":
+		push_temp(2, 2, sensor.get('temp'))
+	elif sensor.get('id') == "th7":
+		push_temp(2, 5, sensor.get('temp'))
+	elif sensor.get('id') == "th8":
+		push_temp(2, 12, sensor.get('temp'))
