6f55bbdfaf959c87d278a82b4a277614c45125c6
BillB <github@billbennett.org>
2018-03-29 12:09:07 -0700

    small refactor

--- bridge/update_isy.py
+++ bridge/update_isy.py
@@ -3,12 +3,7 @@ import xml.etree.ElementTree
 import httplib2
 
 
-def push_temp_isy(variable_type, variable_id, c_temp):
-	#
-	# Convert from celsius to fahrenheit
-	f_temp = round(9.0 / 5.0 * float(c_temp) + 32)
-	url = "http://isy994/rest/vars/set/" + str(variable_type) + "/" + str(variable_id) + "/" + str(f_temp)
-
+def push_temp_isy(variable_type, variable_id, f_temp):
 	#
 	# Get ISY security data
 	secret_file = open("../secret/isy994", "r")
@@ -17,11 +12,18 @@ def push_temp_isy(variable_type, variable_id, c_temp):
 
 	#
 	# do a get on isy994 to update the data
+	url = "http://isy994/rest/vars/set/" + str(variable_type) + "/" + str(variable_id) + "/" + str(f_temp)
 	h = httplib2.Http()
 	h.add_credentials(user_name, password)  # Basic authentication
 	resp, content = h.request(url, "GET")
 	if not str(content).find("<RestResponse succeeded=\"true\"><status>200</status></RestResponse>"):
 		print("Failed URL: ", url, "Response: ", content)
+
+
+def ctof(c_temp):
+	#
+	# Convert from celsius to fahrenheit
+	return round(9.0 / 5.0 * float(c_temp) + 32)
 
 
 def get_meteobridge_xml():
@@ -47,13 +49,13 @@ def update_isy_meteobridge():
 	meteobridge_xml = get_meteobridge_xml()
 	for sensor in meteobridge_xml.findall('TH'):
 		if sensor.get('id') == "th6":
-			push_temp_isy(2, 1, sensor.get('temp'))
+			push_temp_isy(2, 1, ctof(sensor.get('temp')))
 		elif sensor.get('id') == "th0":
-			push_temp_isy(2, 2, sensor.get('temp'))
+			push_temp_isy(2, 2, ctof(sensor.get('temp')))
 		elif sensor.get('id') == "th7":
-			push_temp_isy(2, 5, sensor.get('temp'))
+			push_temp_isy(2, 5, ctof(sensor.get('temp')))
 		elif sensor.get('id') == "th8":
-			push_temp_isy(2, 12, sensor.get('temp'))
+			push_temp_isy(2, 12, ctof(sensor.get('temp')))
 	print("Meteobridge data pushed")
 
 
