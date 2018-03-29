3d55479f9068d99f450696164e4f6a713efa4fe4
BillB <github@billbennett.org>
2018-03-29 16:10:03 -0700

    dealing with warnings.

--- update_isy.py
+++ update_isy.py
@@ -21,13 +21,13 @@ def push_temp_isy(variable_type, variable_id, f_temp):
 	h.add_credentials(user_name, password)  # Basic authentication
 	resp, content = h.request(url, "GET")
 	if not str(content).find("<RestResponse succeeded=\"true\"><status>200</status></RestResponse>"):
-		syslog.syslog(syslog.LOG_EMERG, "Failed URL: ", url, "Response: ", content)
-		print(datetime.datetime.now().time(), " - Failed URL: ", url, "Response: ", content)
+		syslog.syslog(syslog.LOG_EMERG, "Failed URL: " + url + " Response: " + str(content))
+		print(datetime.datetime.now().time(), " - Failed URL: ", url, " Response: ", str(content))
 	else:
 		print(datetime.datetime.now().time(), " - Success URL: ", url)
 
 
-def ctof(c_temp):
+def c_to_f(c_temp):
 	#
 	# Convert from celsius to fahrenheit
 	return round(9.0 / 5.0 * float(c_temp) + 32)
@@ -47,8 +47,8 @@ def get_meteobridge_xml():
 	resp, content = h. \
 		request("http://meteobridge/cgi-bin/livedataxml.cgi", "GET")
 	if resp.status != 200:
-		syslog.syslog(syslog.LOG_EMERG, "Bad response from meteobridge ", str(resp))
-		print(datetime.datetime.now().time(), " -  Bad response from meteobridge.", str(resp))
+		syslog.syslog(syslog.LOG_EMERG, "Bad response from meteobridge " + str(resp))
+		print(datetime.datetime.now().time(), " -  Bad response from meteobridge." + str(resp))
 	xml_response = xml.etree.ElementTree.fromstring(content)
 	return xml_response
 
@@ -59,13 +59,13 @@ def update_isy_meteobridge():
 	meteobridge_xml = get_meteobridge_xml()
 	for sensor in meteobridge_xml.findall('TH'):
 		if sensor.get('id') == "th6":
-			push_temp_isy(2, 1, ctof(sensor.get('temp')))
+			push_temp_isy(2, 1, c_to_f(sensor.get('temp')))
 		elif sensor.get('id') == "th0":
-			push_temp_isy(2, 2, ctof(sensor.get('temp')))
+			push_temp_isy(2, 2, c_to_f(sensor.get('temp')))
 		elif sensor.get('id') == "th7":
-			push_temp_isy(2, 5, ctof(sensor.get('temp')))
+			push_temp_isy(2, 5, c_to_f(sensor.get('temp')))
 		elif sensor.get('id') == "th8":
-			push_temp_isy(2, 12, ctof(sensor.get('temp')))
+			push_temp_isy(2, 12, c_to_f(sensor.get('temp')))
 	syslog.syslog(syslog.LOG_CRIT, "Meteobridge data pushed.")
 
 
