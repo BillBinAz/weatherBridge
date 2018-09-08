6643709c3ba9dd413b3aa3ae7bd6613fdcff6ecc
Bill B <github@billbennett.org>
2018-09-07 22:08:59 -0700

    Used a temp file, 50 second data collection

--- weather433.sh
+++ weather433.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/admin/weatherBridge/data
-rm weather433.json
-rtl_433 -f 433920000 -H 15 -F json:/home/admin/weatherBridge/data/weather433.json -T 10 -R 40 -d 0 -W 2>&1
-
+rm weather433.temp
+rtl_433 -f 433920000 -H 15 -F json:/home/admin/weatherBridge/data/weather433.temp -T 50 -R 40 -d 0 -W 2>&1
+cp weather433.temp weather433.json
