a3725d1ecccb6a7c04b9855506fb7c8f4f26221a
BillB <github@billbennett.org>
2018-09-08 15:27:32 -0700

    parse temp file

--- weather433.sh
+++ weather433.sh
@@ -8,7 +8,7 @@ err() {
 }
 
 # make sure the data directory is current
-cd /home/admin/weatherBridge/data
+cd /home/admin/weatherBridge
 
 # See if it is running
 if ! pgrep -x "rtl_433" > /dev/null
@@ -21,7 +21,7 @@ then
     /usr/local/bin/rtl_433 -F json:/home/admin/weatherBridge/data/weather433.temp -T 300 -R 40 -d 0 -W 2>&1
 
     # now that we have collected data into temp, make it available.
-    cp weather433.temp weather433.json
+    ./filterJsonBySensor.py
 
     # Syslog the success to stderr
     err "weather433.json Updated"
