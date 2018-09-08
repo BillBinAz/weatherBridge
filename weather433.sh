c4dad6a95db3d23e462392765271c1d8b17be7ae
BillB <github@billbennett.org>
2018-09-08 14:36:21 -0700

    collect 5 minutes every 10

--- weather433.sh
+++ weather433.sh
@@ -12,7 +12,7 @@ cd /home/admin/weatherBridge/data
 
 # remove the old temp file
 rm weather433.temp
-/usr/local/bin/rtl_433 -F json:/home/admin/weatherBridge/data/weather433.temp -T 120 -R 40 -d 0 -W 2>&1
+/usr/local/bin/rtl_433 -F json:/home/admin/weatherBridge/data/weather433.temp -T 300 -R 40 -d 0 -W 2>&1
 
 # now that we have collected data into temp, make it available.
 cp weather433.temp weather433.json
