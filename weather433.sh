752a9d08fe9e812982cdd5d0209146c81a06b036
BillB <github@billbennett.org>
2018-09-07 23:01:37 -0700

    full path to rtl_433

--- weather433.sh
+++ weather433.sh
@@ -12,7 +12,7 @@ cd /home/admin/weatherBridge/data
 
 # remove the old temp file
 rm weather433.temp
-rtl_433 -f 433920000 -H 15 -F json:/home/admin/weatherBridge/data/weather433.temp -T 50 -R 40 -d 0 -W 2>&1
+/usr/local/bin/rtl_433 -f 433920000 -H 15 -F json:/home/admin/weatherBridge/data/weather433.temp -T 50 -R 40 -d 0 -W 2>&1
 
 # now that we have collected data into temp, make it available.
 cp weather433.temp weather433.json
