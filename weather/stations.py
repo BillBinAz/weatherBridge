35971a0d45b6162ba6c28fcd00b5c9bba5b98fc6
BillB <github@billbennett.org>
2018-09-14 21:53:09 -0700

    Refactor file locations

--- weather/stations.py
+++ weather/stations.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
-from isy994 import isy994
-from meteohub import meteohub
+import isy994
+import meteohub
 from rtl_433 import weather433
+import get_rest_rtl_433
 from weather import data
 
 
 def get_weather():
 	cur_weather = data.WeatherData()
+	cur_weather = get_rest_rtl_433.get_weather(cur_weather)
 	cur_weather = isy994.get_weather(cur_weather)
 	cur_weather = meteohub.get_weather(cur_weather)
 	cur_weather = weather433.get_weather(cur_weather)
