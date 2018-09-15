40ee61abe2f7d8a960e58798ba58dfb913f1b41e
BillB <github@billbennett.org>
2018-09-14 22:10:05 -0700

    better data merge

--- weather/stations.py
+++ weather/stations.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+import get_rest_rtl_433
 import isy994
 import meteohub
 from rtl_433 import weather433
-import get_rest_rtl_433
 from weather import data
 
 
 def get_weather():
 	cur_weather = data.WeatherData()
-	cur_weather = get_rest_rtl_433.get_weather(cur_weather)
 	cur_weather = isy994.get_weather(cur_weather)
 	cur_weather = meteohub.get_weather(cur_weather)
 	cur_weather = weather433.get_weather(cur_weather)
+	cur_weather = get_rest_rtl_433.get_weather(cur_weather)
 	return cur_weather
 
 
