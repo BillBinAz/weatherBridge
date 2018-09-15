354706872d5be57f2ca988d39068828db1e5cfbd
BillB <github@billbennett.org>
2018-09-15 09:58:03 -0700

    bad path

--- rtl_433/launch_rest.sh
+++ rtl_433/launch_rest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/weatherBridge
-/usr/bin/python3 PYTHONPATH=.:~/weatherBridge/rtl_433 ~/weatherBridge/rtl_433/get_handler.py
+cd ~/weatherBridge/rtl_433
+/usr/bin/python3 ~/weatherBridge/rtl_433/get_handler.py
