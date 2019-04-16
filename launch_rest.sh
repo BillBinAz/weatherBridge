ce52c909f68df6cfeccef70c46ec1d1a80af2450
Bill B <github@billbennett.org>
2019-04-16 07:40:53 -0700

    remove log

--- launch_rest.sh
+++ launch_rest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pgrep python | xargs kill
 cd /home/admin/weatherBridge
-/usr/bin/python3 /home/admin/weatherBridge/get_handler.py &>> /tmp/weatherBridge.getHandler.log 
+/usr/bin/python3 /home/admin/weatherBridge/get_handler.py
