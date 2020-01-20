9a283f55f802f41154209e8e6a6d4d7b7d8da238
BillB <github@billbennett.org>
2020-01-20 15:49:38 -0700

    add main garage and humidity

--- stations/rtl433.py
+++ stations/rtl433.py
@@ -17,6 +17,7 @@ LIVING_ROOM = 'living_room'
 LIVING_ROOM_WINDOW = 'living_room_window'
 MASTER_BEDROOM_THERMOSTAT = 'master_bedroom_thermostat'
 MASTER_BEDROOM_WINDOW = 'master_bedroom_window'
+MAIN_GARAGE = 'main_garage'
 POOL = 'pool'
 SPA = 'spa'
 THEATER = 'theater'
@@ -41,32 +42,12 @@ def get_weather(weather_data, host):
 
 	try:
 		parsed_json = rtl_433_json(host)
-		sensor = parsed_json.get(THEATER_WINDOW)
+		sensor = parsed_json.get(MAIN_GARAGE)
 		temp = sensor.get(TEMPERATURE)
 		if temp != data.DEFAULT_TEMP:
-			weather_data.theater_window.humidity = sensor.get(HUMIDITY)
-			weather_data.theater_window.temp = temp
-			weather_data.theater_window.time = sensor.get(TIME)
-
-		sensor = parsed_json.get(BACK_YARD)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.back_yard.humidity = sensor.get(HUMIDITY)
-			weather_data.back_yard.temp = sensor[TEMPERATURE]
-
-		sensor = parsed_json.get(FRONT_DOOR)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.front_door.humidity = sensor.get(HUMIDITY)
-			weather_data.front_door.temp = sensor[TEMPERATURE]
-			weather_data.front_door.time = sensor.get(TIME)
-
-		sensor = parsed_json.get(LIVING_ROOM_WINDOW)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.living_room_window.humidity = sensor.get(HUMIDITY)
-			weather_data.living_room_window.temp = sensor[TEMPERATURE]
-			weather_data.living_room_window.time = sensor.get(TIME)
+			weather_data.main_garage.humidity = sensor.get(HUMIDITY)
+			weather_data.main_garage.temp = temp
+			weather_data.main_garage.time = sensor.get(TIME)
 
 		sensor = parsed_json.get(HUMIDOR)
 		temp = sensor.get(TEMPERATURE)
@@ -74,52 +55,6 @@ def get_weather(weather_data, host):
 			weather_data.humidor.humidity = sensor.get(HUMIDITY)
 			weather_data.humidor.temp = sensor[TEMPERATURE]
 			weather_data.humidor.time = sensor.get(TIME)
-
-		sensor = parsed_json.get(KITCHEN_THERMOSTAT)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.kitchen_thermostat.temp = sensor[TEMPERATURE]
-
-		sensor = parsed_json.get(LIBRARY)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.library.humidity = sensor.get(HUMIDITY)
-			weather_data.library.temp = sensor[TEMPERATURE]
-			weather_data.library.time = sensor.get(TIME)
-
-		sensor = parsed_json.get(LIVING_ROOM)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.living_room.humidity = sensor.get(HUMIDITY)
-			weather_data.living_room.temp = sensor[TEMPERATURE]
-
-		sensor = parsed_json.get(MASTER_BEDROOM_THERMOSTAT)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.master_bedroom_thermostat.temp = sensor[TEMPERATURE]
-
-		sensor = parsed_json.get(MASTER_BEDROOM_WINDOW)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.master_bedroom_window.humidity = sensor.get(HUMIDITY)
-			weather_data.master_bedroom_window.temp = sensor[TEMPERATURE]
-			weather_data.master_bedroom_window.time = sensor.get(TIME)
-
-		sensor = parsed_json.get(POOL)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.pool.temp = sensor[TEMPERATURE]
-
-		sensor = parsed_json.get(SPA)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.spa.temp = sensor[TEMPERATURE]
-
-		sensor = parsed_json.get(THEATER)
-		temp = sensor.get(TEMPERATURE)
-		if temp != data.DEFAULT_TEMP:
-			weather_data.theater.humidity = sensor.get(HUMIDITY)
-			weather_data.theater.temp = sensor[TEMPERATURE]
 
 	except json.JSONDecodeError as e:
 		syslog.syslog(syslog.LOG_INFO, "Unable to parse kiosk " + e.msg)
