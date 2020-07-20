6c1497db4b67872a005d0192e1042bccea110939
BillB <github@billbennett.org>
2020-07-19 21:10:36 -0700

    Reuse http object between calls

--- update_isy.py
+++ update_isy.py
@@ -20,7 +20,7 @@ AVERAGE_HOUSE_TEMP = 25
 SECRET_FILE = "./secret/isy994"
 
 
-def push_temp_isy(variable_type, variable_id, f_temp, label):
+def push_temp_isy(h, variable_type, variable_id, f_temp, label):
     #
     # Never push defaults to ISY
     if f_temp == data.DEFAULT_TEMP:
@@ -39,9 +39,7 @@ def push_temp_isy(variable_type, variable_id, f_temp, label):
     # do a get on isy994 to update the data
     url = "http://isy994.evilminions.org/rest/vars/set/" + str(variable_type) + "/" + str(variable_id) + "/" + str(
         round(float(f_temp)))
-    h = httplib2.Http()
-    h.add_credentials(user_name, password)  # Basic authentication
-    resp, content = h.request(url, method="GET", headers={'Connection': 'close'})
+    resp, content = h.request(url, "GET")
     if not str(content).find("<RestResponse succeeded=\"true\"><status>200</status></RestResponse>"):
         syslog.syslog(syslog.LOG_INFO, "Failed URL: " + url + " Response: " + str(content))
         print(datetime.datetime.now().time(), " - Failed URL: ", url, " Response: ", str(content))
@@ -49,17 +47,26 @@ def push_temp_isy(variable_type, variable_id, f_temp, label):
         print(datetime.datetime.now().time(), " - Success URL: ", url)
 
 
-def update_isy(weather_data):
-    push_temp_isy(ISY_INTEGER, BACK_YARD_TEMP, weather_data.back_yard.temp, 'BACK_YARD_TEMP')
-    push_temp_isy(ISY_INTEGER, MAIN_GARAGE, weather_data.main_garage.temp, 'MAIN_GARAGE_TEMP')
-    push_temp_isy(ISY_INTEGER, AVERAGE_HOUSE_TEMP, round(weather_data.whole_house_fan.houseTemp), 'AVERAGE_HOUSE_TEMP')
+def update_isy(weather_data, h):
+    push_temp_isy(h, ISY_INTEGER, BACK_YARD_TEMP, weather_data.back_yard.temp, 'BACK_YARD_TEMP')
+    push_temp_isy(h, ISY_INTEGER, MAIN_GARAGE, weather_data.main_garage.temp, 'MAIN_GARAGE_TEMP')
+    push_temp_isy(h, ISY_INTEGER, AVERAGE_HOUSE_TEMP, round(weather_data.whole_house_fan.houseTemp), 'AVERAGE_HOUSE_TEMP')
     syslog.syslog(syslog.LOG_CRIT, "ISY Temps pushed")
 
 
 def main():
     weather_data = stations.get_weather()
     # print(weather_data.to_json())
-    update_isy(weather_data)
+
+    #
+    # Get ISY security data
+    with open(SECRET_FILE, "r") as secret_file:
+        user_name = secret_file.readline().strip('\n')
+        password = secret_file.readline().strip('\n')
+
+    h = httplib2.Http()
+    h.add_credentials(user_name, password)  # Basic authentication
+    update_isy(weather_data, h)
 
 
 main()
