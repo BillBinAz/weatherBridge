c3f49463bca6c2662eb817a8ca4ead2b93c3bc53
BillB <github@billbennett.org>
2020-07-21 18:48:12 -0700

    flask polish

--- get_handler.py
+++ get_handler.py
@@ -9,5 +9,6 @@ app = Flask(__name__)
 @app.route("/weather", methods=['GET'])
 def get_weather():
 	weather_data = stations.get_weather()
+	print("got weather data")
 	return weather_data.to_json(), 200, {'Content-Type': 'text/json; charset=utf-8'}
 
