65c0358f196bb958bdcc98644d383d58b11872f8
bbennett66 <github@billbennett.org>
2023-04-21 16:24:57 -0700

    Default for myq and logging timestamps

--- update_isy.py
+++ update_isy.py
@@ -87,6 +87,8 @@ def update_isy(weather_dict, s, user_name, password):
 def main():
 
     try:
+        logging.basicConfig(format='%(asctime)s %(levelname)s {%(module)s} [%(funcName)s] %(message)s', datefmt='%Y-%m-%d,%H:%M:%S:%f', level=logging.INFO)
+
         #
         # Get weather data from data sources
         if __debug__:
