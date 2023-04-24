62ae35f0ceab4a47d542e05b3ce21ea5b2565f59
Bill B <github@billbennett.org>
2023-04-24 15:03:06 -0700

    Mcclosed (#95)
    * Populate IsRaining in ISY
    
    * Default to closed on myq integration
    
    * Default for myq and logging timestamps
    
    * Default for myq and logging timestamps
    
    * Default for myq and logging timestamps

--- weather/stations.py
+++ weather/stations.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 from stations import isy994, rtl433, airScape, wifiLogger, sensorPush, myq
 from weather import data
+import logging
 
 
 def get_weather():
+
+	logging.basicConfig(format='%(asctime)s %(levelname)s {%(module)s} [%(funcName)s] %(message)s', datefmt='%Y-%m-%d,%H:%M:%S:%f', level=logging.INFO)
+
 	cur_weather = data.WeatherData()
 	isy994.get_weather(cur_weather)
 	wifiLogger.get_weather(cur_weather)
