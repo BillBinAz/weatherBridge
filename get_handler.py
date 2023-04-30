970294a952ccd247bce7b172dda9a71c41b970f6
bbennett66 <github@billbennett.org>
2023-04-30 09:05:19 -0700

    Cleanup of try/catch

--- get_handler.py
+++ get_handler.py
@@ -19,7 +19,7 @@ def get_weather():
     return weather_data.to_json(), 200, {'Content-Type': 'text/json; charset=utf-8'}
 
 
-if __name__ == "__main__":
-    app.debug = False
-    port = int(os.environ.get('PORT', 8080))
-    waitress.serve(app, port=port)
+# if __name__ == "__main__":
+#    app.debug = False
+#    port = int(os.environ.get('PORT', 8080))
+#    waitress.serve(app, port=port)
