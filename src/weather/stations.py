cb7c8bac74d7949db19fe61449a502fa789c6d75
bbennett66 <github@billbennett.org>
2023-12-05 12:25:52 -0700

    Covert tabs to spaces

--- src/weather/stations.py
+++ src/weather/stations.py
@@ -9,26 +9,26 @@ import sys
 
 def get_weather():
 
-	cur_weather = data.WeatherData()
+    cur_weather = data.WeatherData()
 
-	try:
-		logging.basicConfig(format='%(asctime)s %(levelname)s {%(module)s} [%(funcName)s] %(message)s', datefmt='%Y-%m-%d,%H:%M:%S:%f', level=logging.INFO)
+    try:
+        logging.basicConfig(format='%(asctime)s %(levelname)s {%(module)s} [%(funcName)s] %(message)s', datefmt='%Y-%m-%d,%H:%M:%S:%f', level=logging.INFO)
 
-		cur_weather = data.WeatherData()
-		IoX.get_weather(cur_weather)
-		wifiLogger.get_weather(cur_weather)
-		# airScape.get_weather(cur_weather)
-		# rtl433.get_weather(cur_weather, "rtl433.evilminions.org")
-		# myq.get_weather(cur_weather)
-		sensorPush.get_weather(cur_weather)
-		evisalink4_alarm.get_weather(cur_weather)
+        cur_weather = data.WeatherData()
+        IoX.get_weather(cur_weather)
+        wifiLogger.get_weather(cur_weather)
+        # airScape.get_weather(cur_weather)
+        # rtl433.get_weather(cur_weather, "rtl433.evilminions.org")
+        # myq.get_weather(cur_weather)
+        sensorPush.get_weather(cur_weather)
+        evisalink4_alarm.get_weather(cur_weather)
 
-	except Exception as e:
-		logging.error("Unable to get station:get_weather " + str(e))
-		print(dt.datetime.now().time(), "Unable to get station:get_weather " + str(e))
-	except:
-		e = sys.exc_info()[0]
-		logging.error("Unable to get station:get_weather " + str(e))
-		print(dt.datetime.now().time(), "Unable to get station:get_weather " + str(e))
+    except Exception as e:
+        logging.error("Unable to get station:get_weather " + str(e))
+        print(dt.datetime.now().time(), "Unable to get station:get_weather " + str(e))
+    except:
+        e = sys.exc_info()[0]
+        logging.error("Unable to get station:get_weather " + str(e))
+        print(dt.datetime.now().time(), "Unable to get station:get_weather " + str(e))
 
-	return cur_weather
+    return cur_weather
