6b27ee2cebdd9fc39b14502f4395227179039d40
bbennett66 <github@billbennett.org>
2024-01-06 14:18:55 -0700

    Show status

--- src/stations/home_assistant.py
+++ src/stations/home_assistant.py
@@ -156,14 +156,14 @@ def get_thermostat_data(weather_data, bearer_token, s):
     weather_data.hallway_thermostat.heat_set = sensor_data["attributes"]["target_temp_high"]
     weather_data.hallway_thermostat.cool_set = sensor_data["attributes"]["target_temp_low"]
     weather_data.hallway_thermostat.humidity = sensor_data["attributes"]["current_humidity"]
-    weather_data.hallway_thermostat.fan = sensor_data["attributes"]["fan_mode"]
+    weather_data.hallway_thermostat.fan = sensor_data["attributes"]["fan_mode"][:10].title().strip()
     weather_data.hallway_thermostat.temp = sensor_data["attributes"]["current_temperature"]
-    weather_data.hallway_thermostat.state = sensor_data["attributes"]["hvac_action"]
+    weather_data.hallway_thermostat.state = sensor_data["attributes"]["hvac_action"][:10].title().strip()
 
     if sensor_data["state"] == "off":
         weather_data.hallway_thermostat.mode = "Off"
     else:
-        weather_data.hallway_thermostat.mode = sensor_data["attributes"]["preset_mode"]
+        weather_data.hallway_thermostat.mode = sensor_data["attributes"]["preset_mode"][:10].title().strip()
 
 
 def get_weather(weather_data):
